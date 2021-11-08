746e3e4dd8416a8539226c1cfae29eae7c3c2c8c
Cala Walker <you@example.com>
2021-11-08 14:46:43 -0300

    realizamos el js de home en el cual tuvimos que modicar en html y un poco del css. Falta los tres titulos principales con sus respectivas peliculas

--- js/index.js
+++ js/index.js
@@ -10,7 +10,11 @@ const apiKey = "b8b7f0a177fd64911123a0d6c5c6618b";
 // };
 
 //Pagina Home del Sitio
+
+// peliculas populares
 let url = (`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`)
+
+let peliculasPopulares = document.querySelector(".opciones div")
 
 fetch(url)
 
@@ -19,22 +23,96 @@ fetch(url)
     })
 
     .then(function(datos){ 
-        for (let i = 0; i <= 5; i++){ 
+        for (let i = 0; i <= 4; i++){ 
         console.log(datos.results[i]);
         
         //Comenzamos con la estructura de nuestra pagina a partir de innerHTML
 
-            document.querySelector(".divindex").innerHTML += `
+            peliculasPopulares.innerHTML += `
 
+                <article class="divindex" >
+                    <a href="./detail-movie.html" class="titulospelicula"> ${datos.results[i].title}</a>
 
-                            <a href="./detail-movie.html" class="titulospelicula"> ${datos.results[i].title}</a>
+                    <div> <a href="./detail-movie.html"> <img src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].title}" class="portada"> </a> </div>
 
-                            <div> <a href="./detail-movie.html"> <img src="${datos.results[i].backdrop_path}" alt="${datos.results[i].title}" class="portada"> </a> </div>
-
-                            <p>Fecha de Estreno: ${datos.results[i].release_date}</p>
-                    `
+                    <p>Fecha de Estreno: ${datos.results[i].release_date}</p>
+                </article>  
+               `
     }
     })
     .catch(function (error) {
         console.log('el error fue ' + error);
     })
+
+
+//series populares 
+
+let url2 = (`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
+
+let seriesPopulares = document.querySelector(".opciones div")
+
+    fetch(url2)
+
+        .then(function(respuesta){
+            return respuesta.json()
+        })
+
+        .then(function(datos){ 
+            for (let i = 0; i <= 4; i++){ 
+            console.log(datos.results[i]);
+            
+            //Comenzamos con la estructura de nuestra pagina a partir de innerHTML
+
+                seriesPopulares.innerHTML += `
+
+                <article class="divindex">
+                        <a href="./detail-serie.html" class="titulospelicula">${datos.results[i].name}</a>
+                    
+                        <div > <a href="./detail-serie.html"> <img src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].name}" class="portada"> </a> </div>
+                    
+                        <p>Fecha de Estreno:  ${datos.results[i].first_air_date}</p>
+                </article>
+               
+
+                `
+        }
+        })
+        .catch(function (error) {
+            console.log('el error fue ' + error);
+        })
+
+//peliculas mas valoradas
+let url3 = (`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
+
+let peliculasValoradas = document.querySelector(".opciones div")
+
+    fetch(url3)
+
+        .then(function(respuesta){
+            return respuesta.json()
+        })
+
+        .then(function(datos){ 
+            for (let i = 0; i <= 4; i++){ 
+            console.log(datos.results[i]);
+            
+            //Comenzamos con la estructura de nuestra pagina a partir de innerHTML
+
+                peliculasValoradas.innerHTML += `
+
+                <article class="divindex" >
+                    <a href="./detail-movie.html" class="titulospelicula"> ${datos.results[i].title}</a>
+
+                    <div> <a href="./detail-movie.html"> <img src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].title}" class="portada"> </a> </div>
+
+                    <p>Fecha de Estreno: ${datos.results[i].release_date}</p>
+                </article>
+                `
+        }
+        })
+        .catch(function (error) {
+            console.log('el error fue ' + error);
+        })
+
+
+   
