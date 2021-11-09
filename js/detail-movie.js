8137dc2afb302fd287c36b6ebc9741a9731a3a8f
chiarasalvu <csalvucci@udesa.edu.ar>
2021-11-09 16:17:21 -0300

    Avances en el js de detail movie

--- js/detail-movie.js
+++ js/detail-movie.js
@@ -5,16 +5,37 @@ const queryString = location.search
 const queryStringObj = new URLSearchParams(queryString);
 const id = queryStringObj.get("id");
 
-// let urlPelicula = (`https://api.themoviedb.org/3/movie/{movie_id}?api_key=${apiKey}`)
+let urlPelicula = (`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`)
 
 let detallesPeliculas = document.querySelector(".maindetail")
 
-fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`) 
+fetch(urlPelicula) 
 
     .then(function(respuesta){
         return respuesta.json()
     })
 
     .then(function(datos){
-        console.log(datos);
+        for (let i = 0; i < 0 ; i++){ 
+        console.log(datos.results[i])
+
+    ` 
+    <div class="navdetalles">
+        <h3 class="titulodetallepelicula">${datos.results[i].title}</h3>
+        <div> <img src="https://image.tmdb.org/t/p/w342/${datos.results[i].poster_path}" alt="${datos.results[i].title}" class="portada"> </div>
+    </div>
+    <div class="navdetalles">
+        <p>Calificación: ${datos.results[i].vote_average}</p>
+        <p>Fecha de Estreno: ${datos.results[i].release_date}</p> 
+        <p>Duración: ${datos.results[i].runtime}</p>
+        <p> ${datos.results[i].overview}</p>
+        <p>Género: <a href="./detail-genres.html?id=${datos.results[i].id}"> ${datos.results[i].genres}</a></p>
+        <p>Favoritos <i class="icon-star-empty"> </i> </p>
+    </div>
+    `
+    }
     })
+
+    .catch(function (error) {
+        console.log('el error fue ' + error);
+    });
