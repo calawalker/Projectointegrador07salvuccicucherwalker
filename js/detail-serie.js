bfe82df89720ebb575fb7a32fe9e20e65acda84d
Cala Walker <you@example.com>
2021-11-14 14:59:07 -0300

    finalizamos el header con el evento del buscador incluido

--- js/detail-serie.js
+++ js/detail-serie.js
@@ -1,5 +1,30 @@
 const apiKey = "b8b7f0a177fd64911123a0d6c5c6618b";
 
+//Formulario del header 
+
+let formulario = document.querySelector ("form");
+let input = document.querySelector (".campoBuscador")
+let message = document.querySelector (".mensaje")
+
+formulario.addEventListener(`submit`, function (evento){
+   evento.preventDefault();
+   if (input.value.length < 3 ){
+       message.innerText = "Ingresar al menos 3 caracteres"
+   }
+   else { 
+       formulario.submit()
+   }
+   
+})
+
+input.addEventListener ("focus", function(evento){
+   console.log(evento);
+   message.innerText = "";
+   this.value = "";
+})
+
+
+// DETAIL SERIES
 console.log(location.search)
 
 const queryString = location.search
--- js/index.js
+++ js/index.js
@@ -1,13 +1,27 @@
 const apiKey = "b8b7f0a177fd64911123a0d6c5c6618b";
 
-// //Formulario del header 
-// const buscador = document.querySelector("input")
+ //Formulario del header 
 
-// if (buscador.value.length <= 3) {
-//     alert('Debe ingresar mas de tres caracteres');
-// } else {
-//     this.sumbit()
-// };
+ let formulario = document.querySelector ("form");
+ let input = document.querySelector (".campoBuscador")
+ let message = document.querySelector (".mensaje")
+
+formulario.addEventListener(`submit`, function (evento){
+    evento.preventDefault();
+    if (input.value.length < 3 ){
+        message.innerText = "Ingresar al menos 3 caracteres"
+    }
+    else { 
+        formulario.submit()
+    }
+    
+})
+
+input.addEventListener ("focus", function(evento){
+    console.log(evento);
+    message.innerText = "";
+    this.value = "";
+})
 
 //Pagina Home del Sitio
 
