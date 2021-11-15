5dc919a899a27ad71899e1fdcba83f31ae427dff
chiarasalvu <csalvucci@udesa.edu.ar>
2021-11-15 16:30:35 -0300

    Avances en la pagina search results

--- js/search-results.js
+++ js/search-results.js
@@ -26,3 +26,35 @@ input.addEventListener ("focus", function(e){
    mensaje.innerText = "";
      this.value = "";
  })
+
+ //Search results
+ console.log(location.search)
+
+let queryString = location.search
+let queryStringObj = new URLSearchParams(queryString);
+let busqueda = queryStringObj.get("q");
+
+let urlSearch = (`https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${busqueda}`)
+
+let searchResults = document.querySelector(".searchresultarticle")
+
+fetch(urlSearch) 
+
+    .then(function(respuesta){
+        return respuesta.json()
+    })
+
+    .then(function(datos){
+        //console.log(datos)
+        if (datos.results.length > 0) {
+            let resultadoBusqueda = document.querySelector(".titulosearch");
+                resultadoBusqueda.innerHTML += `${busqueda}`
+            console.log(datos)
+            for(let i = 0; i <data.results.length; i++) {
+                if(data.results[i]) {
+
+                }
+            }
+        }
+
+});
