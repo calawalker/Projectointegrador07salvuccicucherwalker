732be6aca39fce99d89b2ab5f7b0b560194e1ebe
Cala Walker <you@example.com>
2021-11-16 20:18:29 -0300

    avances en la pagina de favoritos falta agregar series

--- js/favorites.js
+++ js/favorites.js
@@ -2,38 +2,43 @@ const apiKey = "b8b7f0a177fd64911123a0d6c5c6618b";
 
 //Formulario del header 
 
-let formulario = document.querySelector ("form");
-let input = document.querySelector (".campoBuscador")
-let mensaje = document.querySelector (".mensaje")
+let formulario = document.querySelector("form");
+let input = document.querySelector(".campoBuscador")
+let mensaje = document.querySelector(".mensaje")
 
-formulario.addEventListener(`submit`, function (e){
-   e.preventDefault();
-   if (input.value == ""){
-       mensaje.innerText = "El campo esta vacio"
-   }
+formulario.addEventListener(`submit`, function (e) {
+    e.preventDefault();
+    if (input.value == "") {
+        mensaje.innerText = "El campo esta vacio"
+    }
 
-   else if (input.value.length < 3 ){
-       mensaje.innerText = "Ingresar al menos 3 caracteres"
-   }
-   else { 
-       formulario.submit()
-   }
-   
+    else if (input.value.length < 3) {
+        mensaje.innerText = "Ingresar al menos 3 caracteres"
+    }
+    else {
+        formulario.submit()
+    }
+
 })
 
-input.addEventListener ("focus", function(e){
-  console.log(e);
-  mensaje.innerText = "";
+input.addEventListener("focus", function (e) {
+    console.log(e);
+    mensaje.innerText = "";
     this.value = "";
 })
 
-input.addEventListener ("focus", function(evento){
-   console.log(evento);
-   message.innerText = "";
-   this.value = "";
+input.addEventListener("focus", function (evento) {
+    console.log(evento);
+    message.innerText = "";
+    this.value = "";
 })
 
 //Comenzamos con favoritos
+console.log(location.search)
+
+const queryString = location.search
+const queryStringObj = new URLSearchParams(queryString);
+const id = queryStringObj.get("id");
 
 let favoritos = []
 
@@ -41,17 +46,17 @@ if (localStorage.getItem("favoritos")) {
 
     console.log(localStorage);
 
-let recuperoStorage = localStorage.getItem("favoritos")
+    let recuperoStorage = localStorage.getItem("favoritos")
 
-favoritos = JSON.parse(recuperoStorage);
+    favoritos = JSON.parse(recuperoStorage);
 
-console.log(favoritos);
+    console.log(favoritos);
 
 }
 
-const seccion = document.querySelector(".favoritosgeneral")
+const seccion = document.querySelector(".opciones div")
 
-if( favoritos.length == 0) {
+if (favoritos.length == 0) {
 
     seccion.innerHTML += `
     <article>
@@ -60,45 +65,72 @@ if( favoritos.length == 0) {
     </article>
     `
 }
-else{
+else {
 
-for (let i = 0; i < favoritos.length; i++) {
+    for (let i = 0; i < favoritos.length; i++) {
 
-    busquedaYmuestraFavoritosMovie (favoritos[i] );
-    busquedaYmuestraFavoritosSerie (favoritos[i]);
+        busquedaYmuestraFavoritosMovie(favoritos[i]);
+        // busquedaYmuestraFavoritosSerie(favoritos[i]);
+    }
+
 }
-
-}
-function  busquedaYmuestraFavoritosMovie ( id ) {
+function busquedaYmuestraFavoritosMovie(id) {
 
     let urlFav = `https://api.themoviedb.org/3/movie/${id}?api_key=${apiKey}`
 
     fetch(urlFav)
-    
-    .then ( function(respuesta){
-        return respuesta.json();
-    })
-    .then ( function (datos) {
-        console.log(datos)
 
-        // let info = datos.
-        // console.log(info);
-    })
+        .then(function (respuesta) {
+            return respuesta.json();
+        })
+        .then(function (datos) {
+             console.log(datos)
+
+             seccion.innerHTML +=
+                `
+                <article class="divindex" >
+                     <a href="./detail-movie.html?id=${datos.id}" class="titulospelicula"> ${datos.title}</a>
+
+                     <div> <a href="./detail-movie.html?id=${datos.id}"> <img src="https://image.tmdb.org/t/p/w342/${datos.poster_path}" alt="${datos.title}" class="portada"> </a> </div>
+
+                    <p>Fecha de Estreno: ${datos.release_date}</p>
+                </article> `
+        
+
+        })
 }
+            // for (let i = 0; i < favoritos.results; i++) {
 
-function  busquedaYmuestraFavoritosSerie ( ids) {
+            //     seccion.innerHTML +=
+            //         `
+            //    <article class="divindex" >
+            //         <a href="./detail-movie.html?id=${datos[i].id}" class="titulospelicula"> ${datos[i].title}</a>
 
-    let urlFavS = `https://api.themoviedb.org/3/tv/${ids}?api_key=${apiKey}`
+            //         <div> <a href="./detail-movie.html?id=${datos[i].id}"> <img src="https://image.tmdb.org/t/p/w342/${datos[i].poster_path}" alt="${datos[i].title}" class="portada"> </a> </div>
 
-    fetch(urlFavS)
-    
-    .then ( function(respuesta){
-        return respuesta.json();
-    })
-    .then ( function (datos) {
-        console.log(datos)
+            //         <p>Fecha de Estreno: ${datos[i].release_date}</p>
+            //     </article> `
 
-        // let info = datos.
-        // console.log(info);
-    })
-}
+            // }
+
+
+
+
+
+
+// function busquedaYmuestraFavoritosSerie(ids) {
+
+//     let urlFavS = `https://api.themoviedb.org/3/tv/${id}?api_key=${apiKey}`
+
+//     fetch(urlFavS)
+
+//         .then(function (respuesta) {
+//             return respuesta.json();
+//         })
+//         .then(function (datos) {
+//             console.log(datos)
+
+//             // let info = datos.
+//             // console.log(info);
+//         })
+// }
